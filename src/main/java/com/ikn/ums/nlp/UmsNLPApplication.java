8bb9263368041dcce38a3e7cfcaa63faa5da4aff
Vaishnav Puthiyanamparambath <vaishnav.p@ikcontech.com>
2023-09-13 17:46:54 +0530

    rest template added in main class

--- src/main/java/com/ikn/ums/nlp/UmsNLPApplication.java
+++ src/main/java/com/ikn/ums/nlp/UmsNLPApplication.java
@@ -2,12 +2,21 @@ package com.ikn.ums.nlp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
  class UmsNLPApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UmsNLPApplication.class, args);
+	}
+	
+	@LoadBalanced
+	@Bean
+	public RestTemplate createLoadBalancedRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
